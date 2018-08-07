48a8d0fb3bd605adef1fdfaa09528a357549e86b
Chan Lee and Regina Nurieva <regina@>
2018-08-07 13:42:22 -0700

    apply js function to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-  $("button#blue").click(function() {
-    $("body").addClass("dark-background");
-  });
-  $("button#white").click(function() {
-    $("body").removeClass();
+  $("li").click(function() {
+    $(this).children("p").fadeToggle();
   });
 });
